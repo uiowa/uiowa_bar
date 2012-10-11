32eb11e1ced399ed71add31de20a4bfc0eb374b5
Brandon Neil <brandon-neil@uiowa.edu>
2012-10-11 15:48:56 -0500

    Use a render array to build the iowa_bar

--- theme/uiowa-bar.tpl.php
+++ theme/uiowa-bar.tpl.php
@@ -8,45 +8,16 @@
  *
  *
  */
-
-// Base URL without the protocol - needed for the Google Search Appliance
-$base_url_no_protocol = preg_replace('/^https?:\/\//i', '', $GLOBALS['base_url']);
 ?>
 
-<div id="ui-wrapper"><div class="container">
-  <div id="ui-global-bar">
-    <div id="ui-logo"><a href="http://www.uiowa.edu">The University of Iowa</a></div>
-    <div id="ui-global-bar-nav">
-      <div id="ui-global-links">
-        <ul>
-          <li><a href="http://www.uiowa.edu/homepage/directories/" class="icon phonebook" title="Phonebook">Phonebook</a></li>
-          <li><a href="http://www.uiowa.edu/homepage/hub/tours.html" class="icon maps" title="Maps">Maps</a></li>
-          <li><a href="http://www.uiowa.edu/homepage/search/index.html" class="icon a-z" title="A-Z">A-Z</a></li>
-          <li><a href="#" class="icon search" title="Search">Search</a></li>
-        </ul>
-      </div><!-- /ui-global-links -->
-      <div id="gsa-search">
-        <form action="http://search.uiowa.edu/search" method="get">
-          <fieldset>
-            <legend class="element-hidden">Search</legend>
-            <div id="search-box">
-              <label class="element-hidden" for="search-terms">Search Terms</label>
-              <input id="search-terms" maxlength="256" name="q" placeholder="Start Searching ..." size="15" type="text" value="" />
-              <label for="sitesearch" id="site-search-label">for</label><div id="sitesearch-container">
-              <select id="sitesearch" name="sitesearch">
-                <option id="search-this-site" value="<?php print $base_url_no_protocol ?>">This Site</option>
-                <option id="search-ui" value="www.uiowa.edu">All Sites</option>
-              </select>
+<?php
 
-              <input id="submit-search" name="btnG" type="submit" value="Search" />
-              <input name="site" type="hidden" value="default_collection" />
-              <input name="client" type="hidden" value="default_frontend" />
-              <input name="output" type="hidden" value="xml_no_dtd" />
-              <input name="proxystylesheet" type="hidden" value="default_frontend" />
-            </div><!-- /search-box -->
-          </fieldset>
-        </form>
-      </div><!-- /gsa-search -->
-    </div><!-- /global-bar-nav -->
-  </div><!-- /ui-global-bar -->
-  </div></div><!-- /ui-wrapper and conatiner -->
+dpm($uiowa_bar, "UIowa Bar");
+dpm(array_keys(theme_get_registry()));
+?>
+
+<div id="ui-wrapper">
+  <div class="container">
+    <?php print render($uiowa_bar); ?>
+  </div>
+</div><!--ui-wrapper and conatiner -->
