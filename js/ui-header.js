247b8da4ff94531efe67e9680ac13938e7f381f1
Brandon Neil <brandon-neil@uiowa.edu>
2012-10-25 11:08:35 -0500

    Initial commit for mobile first uiowa_bar

--- js/ui-header.js
+++ js/ui-header.js
@@ -1,15 +1,16 @@
 (function ($) {
    $(document).ready(function() {
-	   // Use uniform.js library to customize select box in ui-header
-	   $("#ui-global-bar select").uniform();
-	   
-	   $("#ui-global-bar #search-box").hide().toggleClass("collapsed"); 
-	   $("#ui-global-bar a.search").click(function (evt) { 
-		  evt.preventDefault();
-		  
-		  
-		  $("#ui-global-bar #search-box").css('height', '28px').css('overflow', 'hidden').toggle('slow');
-		  $(this).toggleClass("expanded"); 
-	   });
+     // Use uniform.js library to customize select box in ui-header
+     $("#ui-global-bar select").uniform();
+
+     $("#ui-global-bar #gsa-search").hide().toggleClass("collapsed");
+     $("#ui-global-bar a.search").click(function (evt) {
+      evt.preventDefault();
+      $("#ui-global-bar #gsa-search")
+      //.css('height', '28px')
+        //.css('overflow', 'hidden')
+        .toggle()
+        .toggleClass("expanded").toggleClass("collapsed");
+     });
    });
 }(jQuery));
