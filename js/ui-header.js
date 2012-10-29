11a3c2370524f7a79c1b3de90146a5742d9c4eab
Brandon Neil <brandon-neil@uiowa.edu>
2012-10-29 13:20:10 -0500

    Added a display option for the search form to on wide displays

--- js/ui-header.js
+++ js/ui-header.js
@@ -12,5 +12,14 @@
         .toggle()
         .toggleClass("expanded").toggleClass("collapsed");
      });
+
+      var mql = window.matchMedia("screen and (min-width: 700px)");
+
+      /* If the University of Iowa branding bar is set to close for wide screen
+      devices, then close. */
+      if (mql.matches && Drupal.settings.uiowa_bar.uiowa_bar_search_display === 0) {
+
+          $("#ui-global-bar #gsa-search").toggle().toggleClass("expanded");
+      }
    });
 }(jQuery));
