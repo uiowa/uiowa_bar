f9f8e7f4ed6ac6838d2c4713453441fe2123ef35
Brandon Neil <brandon-neil@uiowa.edu>
2012-10-30 15:37:56 -0500

    Introduce nab icon for sm screen text-style link display

--- js/ui-header.js
+++ js/ui-header.js
@@ -12,6 +12,22 @@
       .toggleClass("expanded").toggleClass("collapsed");
     });
 
+    if (Drupal.settings.uiowa_bar.uiowa_bar_link_style === 'uiowa-text') {
+      $("#ui-global-links").before("<div id='ui-global-nav-sm'><a href='#'>Navigation</a></div>");
+      $("#ui-global-links").hide().toggleClass("collapsed");
+      $("#ui-global-nav-sm a").click(function (evt) {
+        evt.preventDefault();
+        $("#ui-global-links")
+        .toggle()
+        .toggleClass("expanded").toggleClass("collapsed");
+        $("#ui-global-bar #gsa-search")
+        .toggle()
+        .toggleClass("expanded").toggleClass("collapsed");
+      });
+      $search = $("#ui-global-links li.last");
+      $search.detach();
+    }
+
     var mql700 = window.matchMedia("screen and (min-width: 700px)");
 
     /* If the search display is set to open for wide screen
@@ -21,5 +37,14 @@
     }
 
     var mql820 = window.matchMedia("screen and (min-width: 820px)");
+
+    if (mql820.matches && Drupal.settings.uiowa_bar.uiowa_bar_link_style == "uiowa-text") {
+      $("#ui-global-nav-sm").detach();
+      $("#ui-global-links")
+        .toggle()
+        .toggleClass("expanded").toggleClass("collapsed");
+      $("#ui-global-links ul").append($search);
+    }
+
   });
 }(jQuery));
