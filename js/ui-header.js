e0509fa16e3a637c66062208d2a8859f3327d249
Adam Delaney <adam-delaney@uiowa.edu>
2013-02-07 13:48:10 -0600

    added js toggles for responsive and resolved x-browser issues

--- js/ui-header.js
+++ js/ui-header.js
@@ -1,59 +1,29 @@
 (function ($) {
-    Drupal.uiowa_bar = function() {
-    // Use uniform.js library to customize select box in ui-header
-    /*$("#ui-global-bar select").uniform();
-    $("#ui-global-bar #gsa-search").hide().toggleClass("collapsed");
-    $("#ui-global-bar a.search").click(function (evt) {
+  Drupal.uiowa_bar = function() {
+    $("#ui-search-toggle").addClass("inactive");
+    $("#ui-search").addClass("search-hidden");
+
+    $("#ui-search-toggle").click(function (evt) {
       evt.preventDefault();
-      $("#ui-global-bar #gsa-search")
-      //.css('height', '28px')
-      //.css('overflow', 'hidden')
-      .toggle()
-      .toggleClass("expanded").toggleClass("collapsed");
+
+      if ($(this).hasClass('inactive')) {
+        $(this).addClass('active').removeClass('inactive');
+        $('#ui-search').addClass('search-exposed').removeClass('search-hidden');
+      } else if ($(this).hasClass('active')) {
+        $(this).addClass('inactive').removeClass('active');
+        $('#ui-search').addClass('search-hidden').removeClass('search-exposed');
+      }
+
     });
-
-    if (Drupal.settings.uiowa_bar.uiowa_bar_link_style === 'uiowa-text') {
-      $("#ui-global-bar-nav").before("<div id='ui-global-nav-sm'><a href='#'>Navigation</a></div>");
-      $("#ui-global-links").hide().toggleClass("collapsed");
-      $("#ui-global-nav-sm a").click(function (evt) {
-        evt.preventDefault();
-        $("#ui-global-links")
-        .toggle()
-        .toggleClass("expanded").toggleClass("collapsed");
-        $("#ui-global-bar #gsa-search")
-        .toggle()
-        .toggleClass("expanded").toggleClass("collapsed");
-      });
-      $search = $("#ui-global-links li.last");
-      $search.detach();
-    }
-
-    var mql700 = window.matchMedia("screen and (min-width: 700px)");*/
-
-    /* If the search display is set to open for wide screen
-    devices, then expand. */
-    /*if (mql700.matches && Drupal.settings.uiowa_bar.uiowa_bar_search_display === 1) {
-      $("#ui-global-bar #gsa-search").toggle().toggleClass("expanded");
-    }
-
-    var mql820 = window.matchMedia("screen and (min-width: 820px)");
-
-    if (mql820.matches && Drupal.settings.uiowa_bar.uiowa_bar_link_style == "uiowa-text") {
-      $("#ui-global-nav-sm").detach();
-      $("#ui-global-links")
-        .toggle()
-        .toggleClass("expanded").toggleClass("collapsed");
-      $("#ui-global-links ul").append($search);
-    }*/
   };
 
   // Attach uiowa_bar behavior.
   Drupal.behaviors.uiowa_bar = {
-    /*attach: function(context, settings) {
-      $('#ui-global-bar', context).once('uiowa_bar', function() {
+    attach: function(context, settings) {
+      $('#ui-wrapper', context).once('uiowa_bar', function() {
         Drupal.uiowa_bar();
       });
-    }*/
+    }
   };
 
 })(jQuery);
