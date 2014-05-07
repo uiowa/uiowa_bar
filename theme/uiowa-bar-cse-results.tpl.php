0538884945d38a2adffb08b28c739790c237cd9c
Mark Ahrens <mark-ahrens@uiowa.edu>
2014-05-07 14:32:58 -0500

    added php to the php tags

--- theme/uiowa-bar-cse-results.tpl.php
+++ theme/uiowa-bar-cse-results.tpl.php
@@ -2,6 +2,6 @@
 $queryString = drupal_get_query_parameters();
 ?>
 
-<h3 id="university-search"><a href="http://search.uiowa.edu/search?q=<? echo $queryString['search'];?>" target="_blank">Search the University of Iowa for <em><? echo $queryString['search'];?></em></a></h3>
+<h3 id="university-search"><a href="http://search.uiowa.edu/search?q=<?php echo $queryString['search'];?>" target="_blank">Search the University of Iowa for <em><?php echo $queryString['search'];?></em></a></h3>
 
 <div id="search-results"></div>
