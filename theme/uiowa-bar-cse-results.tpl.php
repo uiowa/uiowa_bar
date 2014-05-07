9c692cb1aac99138115cb30ba46fc0c8ecb4c51f
Mark Ahrens <mark-ahrens@uiowa.edu>
2014-05-07 13:49:08 -0500

    Changed _GET to drupal_get_query_parameters

--- theme/uiowa-bar-cse-results.tpl.php
+++ theme/uiowa-bar-cse-results.tpl.php
@@ -1,4 +1,41 @@
-<h3 id="university-search"><a href="http://search.uiowa.edu/search?q=<?=$_GET['search'];?>" target="_blank">Search the University of Iowa for <em><?=$_GET['search'];?></em></a></h3>
+<?php
+$queryString = drupal_get_query_parameters();
+?>
+
+<h3 id="university-search"><a href="http://search.uiowa.edu/search?q=<?=$queryString['search'];?>" target="_blank">Search the University of Iowa for <em><?=$queryString['search'];?></em></a></h3>
 
 <div id="search-results"></div>
 
+<script>
+    
+var myCallback = function() {
+  google.search.cse.element.render(
+  {
+      div: "search-results",
+      tag: 'search',
+      attributes:{
+        queryParameterName:'search',
+        <?php
+          if(variable_get('uiowa_bar_cse_scope') == true){ ?>
+            as_sitesearch:'<?=$GLOBALS["base_url"]?>',
+          <?}?>
+        
+      }
+  });
+};
+window.__gcse = {
+  parsetags: 'explicit',
+  callback: myCallback
+};
+
+(function() {
+  var cx = '<?=variable_get("uiowa_bar_cse_engine_id")?>'; // Insert your own Custom Search engine ID here
+  var gcse = document.createElement('script'); gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = (document.location.protocol == 'https' ? 'https:' : 'http:') +
+      '//www.google.com/cse/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+})();
+    
+
+</script>
