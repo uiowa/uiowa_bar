eb28a7a166234e0ea90fbb713fd86e71e3251080
Mark Ahrens <mark-ahrens@uiowa.edu>
2014-05-07 13:29:57 -0500

    Removed link to search University sites to see if it fixes a white screen issue.

--- theme/uiowa-bar-cse-results.tpl.php
+++ theme/uiowa-bar-cse-results.tpl.php
@@ -1,4 +1,3 @@
-<h3 id="university-search"><a href="http://search.uiowa.edu/search?q=<?=$_GET['search'];?>" target="_blank">Search the University of Iowa for <em><?=$_GET['search'];?></em></a></h3>
 
 <div id="search-results"></div>
 
