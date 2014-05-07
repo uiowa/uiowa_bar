5c65325085839176a6aae27db9b77fd2e5fd67a1
Mark Ahrens <mark-ahrens@uiowa.edu>
2014-05-07 14:39:47 -0500

    Editing part of the js to see if I can get any of it to load

--- theme/uiowa-bar-cse-results.tpl.php
+++ theme/uiowa-bar-cse-results.tpl.php
@@ -16,26 +16,13 @@ var myCallback = function() {
       attributes:{
         queryParameterName:'search',
         <?php
-          if(variable_get('uiowa_bar_cse_scope') == true){ ?>
-            as_sitesearch:'<?php echo $GLOBALS["base_url"];?>',
-          <?}?>
+          if(variable_get('uiowa_bar_cse_scope') == true){
+            echo('as_sitesearch:'.$GLOBALS["base_url"].','); 
+          }?>
         
       }
   });
 };
-window.__gcse = {
-  parsetags: 'explicit',
-  callback: myCallback
-};
 
-(function() {
-  var cx = '<?php echo variable_get("uiowa_bar_cse_engine_id");?>'; // Insert your own Custom Search engine ID here
-  var gcse = document.createElement('script'); gcse.type = 'text/javascript';
-  gcse.async = true;
-  gcse.src = (document.location.protocol == 'https' ? 'https:' : 'http:') +
-      '//www.google.com/cse/cse.js?cx=' + cx;
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
-})();
-    
 
 </script>
