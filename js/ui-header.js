85287dc2a9d2ab905bc6ad3bf317ba88abc1260b
Richard B. Porter <richard-b-porter@uiowa.edu>
2015-06-15 15:25:55 -0500

    wrap if/else with brackets

--- js/ui-header.js
+++ js/ui-header.js
@@ -12,10 +12,12 @@
     $.fn.toggleAttr = function(attr, attr1, attr2) {
       return this.each(function() {
         var self = $(this);
-        if (self.attr(attr) == attr1)
+        if (self.attr(attr) == attr1) {
           self.attr(attr, attr2);
-        else
+        }
+        else {
           self.attr(attr, attr1);
+        }
       });
     };
 
