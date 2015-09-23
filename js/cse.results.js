7b07be000e609e6a3019f23905f997ea9b363cb4
Mark Ahrens <mark-ahrens@uiowa.edu>
2015-09-23 12:46:33 -0500

    Updating the CSE URL

--- js/cse.results.js
+++ js/cse.results.js
@@ -34,7 +34,7 @@
           var gcse = document.createElement('script'); gcse.type = 'text/javascript';
           gcse.async = true;
           gcse.src = (document.location.protocol == 'https' ? 'https:' : 'http:') +
-              '//www.google.com/cse/cse.js?cx=' + cx;
+              '//cse.google.com/cse.js?cx=' + cx;
           var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
         })();
       });
